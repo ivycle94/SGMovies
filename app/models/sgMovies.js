fd6e59b795d2dc2bc0120ff33d3e3040c2dc2afb
Ivycle94 <Ivycle94@gmail.com>
2022-03-29 22:49:26 -0500

    inital commit

---
+++ app/models/sgMovies.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const sgMoviesSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			// required: true,
+		},
+		description: {
+			type: String,
+			// required: true,
+		},
+		year: {
+			type: Number,
+			// required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('SGMovies', sgMoviesSchema)
